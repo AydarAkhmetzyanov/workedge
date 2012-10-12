981473af7e0e65ecf6452bdce34e76db28607b86
aydar <aydar@creativestripe.ru>
2012-10-12 06:10:16 +0400

    file download started

--- MVC/controllers/library_wall_ajax.php
+++ MVC/controllers/library_wall_ajax.php
@@ -3,7 +3,21 @@
 class Library_wall_ajaxController extends Controller {
     
 	public function getFile($target, $fileId){
-	    
+	    if (file_exists($file)) {
+            if (ob_get_level()) {
+                ob_end_clean();
+            }
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename=' . basename($file));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            readfile($file);
+            exit;
+        }
 	}
 	
 	public function wallPost($target, $child){
